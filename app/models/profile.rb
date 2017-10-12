f32d7b4e0925de543d034744a87ae7e8303a7a30
Kavindu Amarasinghe <kamarasinghe@nevada.unr.edu>
2017-10-12 20:57:34 +0000

    Added avatar column to profiles table

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,9 @@
 class Profile < ActiveRecord::Base
   belongs_to :user
+  # Create relationship between images and profiles
+  # Paperclip calls them avatars
+  has_attached_file :avatar,
+                    :styles => { :medium => "300x300>", :thumb => "100x100>" },
+                    :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end
