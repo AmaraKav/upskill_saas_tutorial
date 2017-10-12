a4142d67cb3772abe21a96a515a6b4915e67769e
Kavindu Amarasinghe <kamarasinghe@nevada.unr.edu>
2017-10-12 05:18:57 +0000

    Added user profiles form

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,6 +3,7 @@ class ProfilesController < ApplicationController
   # When user makes GET to /users/user_idprofile/new
   def new
     # Render blank profile details form
+    @profile = Profile.new
   end
   
 end
