13bb5d80de934ccccb4e3c43dd59467dd23737b0
Kavindu Amarasinghe <kamarasinghe@nevada.unr.edu>
2017-10-14 19:52:34 +0000

    Added users index action and view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
   
+  def index 
+    
+  end
   # Index is viewing all the objects (users)
   # Show is viewing just one object 
   # When GET request to /users/:id
